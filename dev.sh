09cf221fa8cc3b681f600afba8a1343990100ca2
Judson Neer <judson.neer@gmail.com>
2019-04-17 15:08:04 -0700

    Move dev-specific install items from macos to dev script.

--- dev.sh
+++ dev.sh
@@ -34,3 +34,21 @@ sudo gem install travis --no-rdoc --no-ri
 # XCode
 xcode-select --install
 sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
+
+
+# AWS environment helper
+wget -O aws-env https://raw.githubusercontent.com/fpco/devops-helpers/master/aws/aws-env.sh
+chmod a+x aws-env
+mv aws-env /usr/local/bin/
+
+# AWS credential helper for EKS
+wget -O aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.11.5/2018-12-06/bin/darwin/amd64/aws-iam-authenticator
+chmod a+x aws-iam-authenticator
+mv aws-iam-authenticator /usr/local/bin/
+
+# Terraform
+wget -O terraform.zip https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_darwin_amd64.zip
+unzip terraform.zip
+chmod a+x aws-iam-authenticator
+mv terraform /usr/local/bin/
+rm terraform.zip
--- macos.sh
+++ macos.sh
@@ -208,6 +208,7 @@ defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 # ... but not internal hard drives or mounted servers
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
+defaults write com.apple.finder ShowConnectedServersOnDesktop -bool false
 
 # Finder: show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
@@ -476,28 +477,6 @@ defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
 # Expand the print dialog by default
 defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
 defaults write com.google.Chrome.canary PMPrintingExpandedStateForPrint2 -bool true
-
-
-###############################################################################
-# Development Tools                                                           #
-###############################################################################
-
-# Install aws-env helper
-wget -O aws-env https://raw.githubusercontent.com/fpco/devops-helpers/master/aws/aws-env.sh
-chmod a+x aws-env
-mv aws-env /usr/local/bin/
-
-# Install AWS credential helper for EKS
-wget -O aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.11.5/2018-12-06/bin/darwin/amd64/aws-iam-authenticator
-chmod a+x aws-iam-authenticator
-mv aws-iam-authenticator /usr/local/bin/
-
-# Install terraform
-wget -O terraform.zip https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_darwin_amd64.zip
-unzip terraform.zip
-chmod a+x aws-iam-authenticator
-mv terraform /usr/local/bin/
-rm terraform.zip
 
 
 echo "Done. Note that some of these changes require a logout/restart to take effect."
